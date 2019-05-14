84420e53cdc5dc59ab4e5e5e1b41ceca6d96e64e
watanata <watanabetatsuya@lifull.com>
2019-05-14 16:58:06 +0900

    add page for links and modify show page

---
+++ pages/links.vue
@@ -0,0 +1,3 @@
+<template>
+  <nuxt-link to='/show?name=<img%20src%3D"hoge"%20onerror%3Dalert%281%29>'>DOM-Based-XSS</nuxt-link>
+</template>
--- pages/show.vue
+++ pages/show.vue
@@ -6,7 +6,9 @@
 <script>
 export default {
   async asyncData(context) {
-    context.res.setHeader("X-XSS-Protection","0")
+    if(context.isServer) {
+      context.res.setHeader("X-XSS-Protection","0")
+    }
     return { name: context.query.name };
   }
 };
