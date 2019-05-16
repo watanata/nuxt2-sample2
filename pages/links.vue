a26797fac1fb5ec6fff69f41e5f86bace111208c
watanata <watanabetatsuya@lifull.com>
2019-05-16 10:17:02 +0900

    modify URL list page

--- pages/links.vue
+++ pages/links.vue
@@ -1,3 +1,15 @@
 <template>
-  <nuxt-link to='/show?name=<img%20src%3D"hoge"%20onerror%3Dalert%281%29>'>DOM-Based-XSS</nuxt-link>
+  <div>
+    <ul>
+      <li>
+        <nuxt-link to='/show?name=<img%20src%3D"hoge"%20onerror%3Dalert%281%29>'>DOM-Based-XSS</nuxt-link>
+      </li>
+      <li>
+        <nuxt-link to='/show?name=<img src="hoge" onerror=alert(1)>'>DOM-Based-XSS 2</nuxt-link>
+      </li>
+      <li>
+        <nuxt-link to='/show?name=<img src="hoge" onerror=alert(document.cookie)>'>DOM-Based-XSS 3</nuxt-link>
+      </li>
+    </ul>
+  </div>
 </template>
