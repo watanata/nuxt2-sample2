6acf7d2ebe69a3ca234b7d68f802b75757ab9d03
watanata <watanabetatsuya@lifull.com>
2019-09-05 16:33:21 +0900

    add XSS-able input form page

--- pages/input.vue
+++ pages/input.vue
@@ -1,5 +1,19 @@
 <template>
-  <form action="show">
-    <input type="text" name="name">
-  </form>
+  <div>
+    <p>粗大ごみ受付</p>
+    <form action="" method="POST">
+      氏名 <input type="text" name="name" v-bind:value="name" >
+    </form>
+  </div>
 </template>
+
+<script>
+export default {
+  async asyncData(context) {
+    if(process.server) {
+      context.res.setHeader("X-XSS-Protection","0")
+    }
+    return { name: context.query.name };
+  }
+};
+</script>
