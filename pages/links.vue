0b21b8727a253b7b6193b93542703aa835258c01
watanata <watanata@lifull.com>
2019-09-07 12:50:05 +0900

    move some pages and add a page has xss

--- pages/links.vue
+++ pages/links.vue
@@ -2,13 +2,16 @@
   <div>
     <ul>
       <li>
-        <nuxt-link to='/show?name=<img%20src%3D"hoge"%20onerror%3Dalert%281%29>'>DOM-Based-XSS</nuxt-link>
+        <nuxt-link to='/xss/show?name=<img%20src%3D"hoge"%20onerror%3Dalert%281%29>'>DOM-Based-XSS</nuxt-link>
       </li>
       <li>
-        <nuxt-link to='/show?name=<img src="hoge" onerror=alert(1)>'>DOM-Based-XSS 2</nuxt-link>
+        <nuxt-link to='/xss/show?name=<img src="hoge" onerror=alert(1)>'>DOM-Based-XSS 2</nuxt-link>
       </li>
       <li>
-        <nuxt-link to='/show?name=<img src="hoge" onerror=alert(document.cookie)>'>DOM-Based-XSS 3</nuxt-link>
+        <nuxt-link to='/xss/show?name=<img src="hoge" onerror=alert(document.cookie)>'>DOM-Based-XSS 3</nuxt-link>
+      </li>
+      <li>
+        <nuxt-link to='/xss/attr'>属性値を引用符で囲まない場合の脆弱性</nuxt-link>
       </li>
     </ul>
   </div>
---
+++ pages/xss/attr.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div v-html="name">
+    </div>
+    <div>
+      <p>属性値を引用符で囲まない場合の脆弱性</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData(context) {
+    if(process.server) {
+      context.res.setHeader("X-XSS-Protection","0")
+    }
+    return { name: context.query.name };
+  }
+};
+</script>
