13f6a4897c478becf8d2ad07c82a8317bc60615e
watanata <watanabetatsuya@lifull.com>
2019-09-27 18:43:13 +0900

    modify

--- pages/links.vue
+++ pages/links.vue
@@ -1,21 +1,42 @@
 <template>
   <div>
-    <ul>
-      <li>
-        <nuxt-link to='/xss/show?name=<img%20src%3D"hoge"%20onerror%3Dalert%281%29>'>DOM-Based-XSS</nuxt-link>
+    <div>
+      <ul>
+        <li>
+          <nuxt-link to='/xss/show?name=<img%20src%3D"hoge"%20onerror%3Dalert%281%29>'>DOM-Based-XSS</nuxt-link>
+        </li>
+        <li>
+          <nuxt-link to='/xss/show?name=<img src="hoge" onerror=alert(1)>'>DOM-Based-XSS 2</nuxt-link>
+        </li>
+        <li>
+          <nuxt-link to='/xss/show?name=<img src="hoge" onerror=alert(document.cookie)>'>DOM-Based-XSS 3</nuxt-link>
+        </li>
+        <li>
+          <p>リンクを動的に生成する場合の脆弱性 linkパラメータ</p>
+          <p>属性値を引用符で囲まない場合の脆弱性 valueパラメータ</p>
+          <p>属性値を引用符で囲んだ場合の脆弱性 value2パラメータ</p>
+          <nuxt-link to='/xss/attr?link=javascript%3Aalert%28document.cookie%29&value=1%20onmouseover%3Dalert%28document.cookie%29&value2="%20onmouseover%3D"alert%28document.cookie%29'>/xss/attr?link=javascript%3Aalert%28document.cookie%29&value=1%20onmouseover%3Dalert%28document.cookie%29&value2="%20onmouseover%3D"alert%28document.cookie%29</nuxt-link>
+        </li>
+      </ul>
+    </div>
+
+    <div>
+      <li
+         v-for="(route) in routes"
+        >
+        <nuxt-link :to="route.path">{{ route.name }}</nuxt-link>
       </li>
-      <li>
-        <nuxt-link to='/xss/show?name=<img src="hoge" onerror=alert(1)>'>DOM-Based-XSS 2</nuxt-link>
-      </li>
-      <li>
-        <nuxt-link to='/xss/show?name=<img src="hoge" onerror=alert(document.cookie)>'>DOM-Based-XSS 3</nuxt-link>
-      </li>
-      <li>
-        <p>リンクを動的に生成する場合の脆弱性 linkパラメータ</p>
-        <p>属性値を引用符で囲まない場合の脆弱性 valueパラメータ</p>
-        <p>属性値を引用符で囲んだ場合の脆弱性 value2パラメータ</p>
-        <nuxt-link to='/xss/attr?link=javascript%3Aalert%28document.cookie%29&value=1%20onmouseover%3Dalert%28document.cookie%29&value2="%20onmouseover%3D"alert%28document.cookie%29'>/xss/attr?link=javascript%3Aalert%28document.cookie%29&value=1%20onmouseover%3Dalert%28document.cookie%29&value2="%20onmouseover%3D"alert%28document.cookie%29</nuxt-link>
-      </li>
-    </ul>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  async asyncData(context) {
+    console.log()
+    return {
+      routes: context.app.router.options.routes
+    };
+  }
+};
+</script>
--- pages/xss/attr.vue
+++ pages/xss/attr.vue
@@ -3,12 +3,14 @@
     <div class="box">
       <div class="vul">
         <input type="text" name="mail" v-bind:value="value">
-        <p>
-          属性値を引用符で囲まない場合の脆弱性.Vue.jsではv-bindで変数を展開するため、引用符で囲まない書き方ができない。
-        </p>
+        <p>属性値を引用符で囲まない場合の脆弱性.</p>
+        <p>{{ text }} のようなやつ</p>
+        <p>Vue.jsではv-bindで<strong>{{ text2 }}</strong>のように変数を展開するため、引用符で囲まない書き方ができない。</p>
       </div>
       <div class="vul">
         <input type="text" name="sample" v-bind:value="value2">
+        <p>属性値を引用符で囲んだ場合の脆弱性.</p>
+        <p>{{ text3 }} のようなやつ</p>
         <p>
           引用符で囲んだ場合もダブルクォートのエスケープを自動的にやっているため、脆弱性を埋め込めない。
         </p>
@@ -18,15 +20,18 @@
       <div class="vul">
         <a v-bind:href="link">リンク</a>
         <p>ユーザーからの入力をそのままhref属性に適用するとjavascriptを実行可能</p>
+        <p>実装はこれ→ {{ text4 }} </p>
         <p>対策はリンク先のホストをサーバー側で限定することとスキームをhttps, httpに限定することも有効</p>
       </div>
       <div class="vul">
         <a v-if="link.match(/^https?/)" v-bind:href="link">リンク</a>
         <p>対策：aタグに対してv-if="link.match(/^https?/)" でプロトコルを限定しているため、aタグ自体が表示されない</p>
+        <p>実装は→ {{ text5 }}</p>
       </div>
       <div class="vul">
         <nuxt-link v-bind:to="link">nuxt-linkで生成したリンク</nuxt-link>
         <p>相対URLを作成するためjavascript式は実行されない</p>
+        <p>実装は→ {{ text6 }}</p>
       </div>
     </div>
   </div>
@@ -39,10 +44,16 @@ export default {
       context.res.setHeader("X-XSS-Protection","0")
     }
     return {
-      value: context.query.value,
-      value2: context.query.value2,
-      link: context.query.link,
-      raw: context.query.raw
+      value: context.query.value || '1 onmouseover=alert(document.cookie)',
+      value2: context.query.value2 || '" onmouseover="alert(document.cookie)',
+      link: context.query.link1 || 'javascript:alert(document.cookie)',
+      raw: context.query.raw,
+      text: '<input type="text" name="mail" value=<?php echo $_GET[\'value\']?>>',
+      text2: '<input type="text" name="mail" v-bind:value="value">',
+      text3: '<input type="text" name="mail" value="<?php echo $_GET[\'value\']?>">',
+      text4: '<a v-bind:href="link">リンク</a>',
+      text5: '<a v-if="link.match(/^https?/)" v-bind:href="link">リンク</a>',
+      text6: '<nuxt-link v-bind:to="link">nuxt-linkで生成したリンク</nuxt-link>',
     };
   }
 };
